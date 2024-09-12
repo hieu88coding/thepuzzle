f7f19cad32689c2ae9b369678b65adc2b3082571
Hiếu Cao <91723664+hieu88coding@users.noreply.github.com>
2024-09-12 15:42:45 +0700

    fix firebase error

--- frontend/3722ncl/firebase.jsx
+++ frontend/3722ncl/firebase.jsx
@@ -10,7 +10,7 @@ import { getStorage } from "firebase/storage"
 const firebaseConfig = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
+    projectId: "hieucaoport4lio",
     storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: process.env.MESSAGING_SENDER_ID,
     appId: process.env.API_ID,
